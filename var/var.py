cda1f9078b5259ed094d76fa4a6f9b1fde46b9b0
KsushaSeliv <KsenijaSeliv@yandex.ru>
2018-12-27 02:16:10 +0300

    Update var.py

--- var/var.py
+++ var/var.py
@@ -1,1 +1,11 @@
+import random
+import numpy as np
 
+array = np.random.randint(0, 100, 10)
+print(array)  
+
+array1 = list(filter(lambda x: not x%2, array))
+array2 = list(filter(lambda x: x%2, array))
+
+print(array1)
+print(array2)
